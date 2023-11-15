7fcc90a08927e9f0e5da6c1095412fb3907f0122
AndyJunioRR2 <andyvein20@gmail.com>
2023-11-15 16:58:14 -0500

    Ejercicios clase

---
+++ ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo031.java
@@ -0,0 +1,57 @@
+/*
+
+
+ */
+package paquete01;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo031 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        System.out.println("Ingrese su nombre");
+        String nombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido");
+        String apellido = entrada.nextLine();
+        System.out.println("Ingrese su edad ");
+        int edad = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Ingrese su Universidad");
+        String universidad = entrada.nextLine();
+        // nombre = "atacames"
+        char valor = nombre.charAt(0); // obtener el primer caracter de una
+        // cadena
+        //  L   o   j   a
+        //  0   1   2   3
+        switch (valor) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.printf("%s %s con edad %d, es estudiante de la %s"
+                        , nombre.toUpperCase(),
+                        apellido.toUpperCase(), edad, universidad);
+
+            default:
+                System.out.printf("%s %s con edad %d, es estudiante de la %s"
+                        , nombre.toLowerCase(),
+                        apellido.toLowerCase(), edad, universidad);
+                break;
+
+        }
+
+    }
+}
